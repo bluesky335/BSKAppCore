c6df514169d23da7e8267fae0d60b8ccfc13b265
刘万林 <chinabluesky335@gmail.com>
2019-06-25 18:28:56 +0800

    Initial Commit

---
+++ BSKAppCore/BSKAppCore.h
@@ -0,0 +1,19 @@
+//
+//  BSKAppCore.h
+//  BSKAppCore
+//
+//  Created by 刘万林 on 2019/6/25.
+//  Copyright © 2019 cn.liuwanlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BSKAppCore.
+FOUNDATION_EXPORT double BSKAppCoreVersionNumber;
+
+//! Project version string for BSKAppCore.
+FOUNDATION_EXPORT const unsigned char BSKAppCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSKAppCore/PublicHeader.h>
+
+
