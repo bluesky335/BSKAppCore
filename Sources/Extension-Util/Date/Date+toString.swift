c19c5f6606889b3a4727562aa5d1fbb8040bce5b
刘万林 <chinabluesky335@gmail.com>
2019-10-01 01:30:53 +0800

    fix calender local error

--- Sources/Extension-Util/Date/Date+toString.swift
+++ Sources/Extension-Util/Date/Date+toString.swift
@@ -8,15 +8,14 @@
 
 import UIKit
 
-extension Date{
-    enum Format:String{
-        
+extension Date {
+    enum Format: String {
         /// yyyy 完整的年 如：2019
         case year = "yyyy"
-        
+
         /// yy 年的后两位 如：19
         case yearSurfix = "yy"
-        
+
         /// dd 天
         case day = "dd"
         case hour = ""
@@ -24,74 +23,75 @@ extension Date{
 }
 
 public extension BSKExtension where Base == Date {
-    func toString(formate: String) -> String {
+    func toString(formate: String, locale: Locale? = .current) -> String {
         let dateFormate = DateFormatter()
-        dateFormate.locale = Locale.current
+        if let l = locale {
+            dateFormate.locale = l
+        }
         dateFormate.dateFormat = formate
         return dateFormate.string(from: base)
     }
 
     /// "20190608123400"
-    func toPureNumberString()->String{
-        return self.toString(formate: "yyyyMMddHHmmss")
+    func toPureNumberString() -> String {
+        return toString(formate: "yyyyMMddHHmmss")
     }
-    
-    func toString() -> String {
-        
-        let local = BSKLocalization.getLocalStr(table:"DateToString")
-        
-        let calendar = Calendar.current
+
+    func toString(locale: Locale? = .current) -> String {
+        let localStr = BSKLocalization.getLocalStr(table: "DateToString")
+
+        var calendar = Calendar.current
+        calendar.locale = locale
         let dateFormate = DateFormatter()
-        dateFormate.locale = Locale.current
-        
+        if let l = locale {
+            dateFormate.locale = l
+        }
+
         let now = Date()
-        
+
         if calendar.isDateInToday(base) {
             // 今天
-            let compoents = Calendar.current.dateComponents([.minute, .hour, .second], from:now , to:base )
-            if self.base < now {
-             
+            let compoents = calendar.dateComponents([.minute, .hour, .second], from: now, to: base)
+            if base < now {
                 if let h = compoents.hour, h < 0 {
                     // 一小时以上
-                    return "\(abs(h))\(local.localStr(key: "小时以前"))"
+                    return "\(abs(h))\(localStr.localStr(key: "小时以前"))"
                 } else {
                     // 一小时以内
                     if let m = compoents.minute, m < 0 {
-                        return "\(abs(m))\(local.localStr(key:"分钟以前"))"
-                    }else{
-                        return local.localStr(key: "刚刚")
+                        return "\(abs(m))\(localStr.localStr(key: "分钟以前"))"
+                    } else {
+                        return localStr.localStr(key: "刚刚")
                     }
                 }
-            }else{
+            } else {
                 // 今天
-                dateFormate.dateFormat = local.localStr(key: "HH:mm")
-                return dateFormate.string(from: self.base)
+                dateFormate.dateFormat = localStr.localStr(key: "HH:mm")
+                return dateFormate.string(from: base)
             }
         } else if calendar.isDateInYesterday(base) {
             // 昨天
-            dateFormate.dateFormat = local.localStr(key: "昨天 HH:mm") 
-            return dateFormate.string(from: self.base)
+            dateFormate.dateFormat = localStr.localStr(key: "昨天 HH:mm")
+            return dateFormate.string(from: base)
         } else if calendar.isDateInTomorrow(base) {
             // 明天
-            dateFormate.dateFormat = local.localStr(key: "明天 HH:mm")
-            return dateFormate.string(from: self.base)
-        }
-        else{
+            dateFormate.dateFormat = localStr.localStr(key: "明天 HH:mm")
+            return dateFormate.string(from: base)
+        } else {
             let compoents = calendar.dateComponents([.year, .month, .day], from: base, to: Date())
-            if abs(compoents.year ?? 0 ) > 0 {
+            if abs(compoents.year ?? 0) > 0 {
                 // 超过一年
                 dateFormate.dateFormat = "yyyy-MM-dd"
-                return dateFormate.string(from: self.base)
+                return dateFormate.string(from: base)
             } else {
                 // 一年内
                 dateFormate.dateFormat = "MM-dd HH:mm"
-                return dateFormate.string(from: self.base)
+                return dateFormate.string(from: base)
             }
         }
     }
-    
+
     var timestamp: Int {
-        return Int(self.base.timeIntervalSince1970)
+        return Int(base.timeIntervalSince1970)
     }
-    
 }
