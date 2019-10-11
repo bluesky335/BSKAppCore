6dd7fb3bfb694f6dbdeb6fa7101d91de7ca495ff
刘万林 <chinabluesky335@gmail.com>
2019-10-11 08:45:54 +0800

    fix date to string bug

--- Sources/Extension-Util/Date/Date+toString.swift
+++ Sources/Extension-Util/Date/Date+toString.swift
@@ -32,43 +32,42 @@ public extension BSKExtension where Base == Date {
         return dateFormate.string(from: base)
     }
 
-    /// "20190608123400"
+    /// 例如："20190608123400"
     func toPureNumberString() -> String {
         return toString(formate: "yyyyMMddHHmmss")
     }
 
-    func toString(locale: Locale? = .current) -> String {
+    func toString(locale: Locale = .current) -> String {
         let localStr = BSKLocalization.getLocalStr(table: "DateToString")
 
         var calendar = Calendar.current
         calendar.locale = locale
         let dateFormate = DateFormatter()
-        if let l = locale {
-            dateFormate.locale = l
-        }
+        dateFormate.locale = locale
 
         let now = Date()
 
+        let nowInfo = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        let dateInfo = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second], from: base)
+
         if calendar.isDateInToday(base) {
             // 今天
-            let compoents = calendar.dateComponents([.minute, .hour, .second], from: now, to: base)
-            if base < now {
-                if let h = compoents.hour, h < 0 {
-                    // 一小时以上
-                    return "\(abs(h))\(localStr.localStr(key: "小时以前"))"
-                } else {
-                    // 一小时以内
-                    if let m = compoents.minute, m < 0 {
+            let compoents = calendar.dateComponents([.hour, .minute, .second], from: now, to: base)
+
+            if let h = compoents.hour, h == 0 {
+                // 一小时以内
+                if let m = compoents.minute {
+                    if m < 0 {
                         return "\(abs(m))\(localStr.localStr(key: "分钟以前"))"
-                    } else {
+                    } else if m == 0 {
+                        // 一分钟以内
                         return localStr.localStr(key: "刚刚")
                     }
                 }
-            } else {
-                // 今天
-                dateFormate.dateFormat = localStr.localStr(key: "HH:mm")
-                return dateFormate.string(from: base)
             }
+            // 今天
+            dateFormate.dateFormat = localStr.localStr(key: "HH:mm")
+            return dateFormate.string(from: base)
         } else if calendar.isDateInYesterday(base) {
             // 昨天
             dateFormate.dateFormat = localStr.localStr(key: "昨天 HH:mm")
@@ -77,17 +76,16 @@ public extension BSKExtension where Base == Date {
             // 明天
             dateFormate.dateFormat = localStr.localStr(key: "明天 HH:mm")
             return dateFormate.string(from: base)
+        }
+
+        if nowInfo.year != dateInfo.year {
+            // 不同年
+            dateFormate.dateFormat = "yyyy-MM-dd"
+            return dateFormate.string(from: base)
         } else {
-            let compoents = calendar.dateComponents([.year, .month, .day], from: base, to: Date())
-            if abs(compoents.year ?? 0) > 0 {
-                // 超过一年
-                dateFormate.dateFormat = "yyyy-MM-dd"
-                return dateFormate.string(from: base)
-            } else {
-                // 一年内
-                dateFormate.dateFormat = "MM-dd HH:mm"
-                return dateFormate.string(from: base)
-            }
+            // 同一年
+            dateFormate.dateFormat = "MM-dd HH:mm"
+            return dateFormate.string(from: base)
         }
     }
 
