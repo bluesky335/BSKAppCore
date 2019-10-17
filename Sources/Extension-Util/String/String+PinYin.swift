9365f877b231106dd8af7e99bf712b42f6f9b5b8
刘万林 <chinabluesky335@gmail.com>
2019-10-17 21:42:12 +0800

    add pinyin tool

--- Sources/Extension-Util/String/String+PinYin.swift
+++ Sources/Extension-Util/String/String+PinYin.swift
@@ -22,4 +22,17 @@ public extension BSKExtension where Base == String {
         CFStringTransform(mutableString, nil, kCFStringTransformStripDiacritics, false)
         return mutableString.substring(to: 1)
     }
+    /// 获取拼音
+    var PinYin: String {
+        guard self.base.count > 0 else {
+            return self.base
+        }
+        let mutableString = NSMutableString(string: self.base)
+        //        把汉字转为拼音
+        CFStringTransform(mutableString, nil, kCFStringTransformToLatin, false)
+        //        去掉拼音的音标
+        CFStringTransform(mutableString, nil, kCFStringTransformStripDiacritics, false)
+        return mutableString as String
+    }
+    
 }
